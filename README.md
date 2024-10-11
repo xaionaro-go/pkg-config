2d3af7cd4686fed973e408eb1d6cfc6de1564da5
Dmitrii Okunev <xaionaro@dx.center>
2024-10-11 23:20:27 +0100

    Use `--static` to extract required sub-dependencies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # About
 
-This is a minimalistic wrapper that adds `-Wl,-Bstatic` and `-Wl,-Bdynamic` around the `pkg-config --libs` response. This forces to link statically whatever pkg-config returns.
+This is a minimalistic wrapper that adds `--static` to `--libs` requests, and adds `-Wl,-Bstatic` and `-Wl,-Bdynamic` around the response. This forces to link statically whatever pkg-config returns.
 
 Initially I had to implement this tool to make sure the CGo libraries I have as dependencies for my Android application are linked statically, because otherwise [it did not work](https://github.com/fyne-io/fyne/issues/5189).
 
@@ -8,9 +8,7 @@ Initially I had to implement this tool to make sure the CGo libraries I have as 
 
 ```sh
 go install github.com/xaionaro-go/pkg-config-static@latest
-PKG_CONFIG="$HOME/go/bin/go-pkg-config-static"
-
-go build PATH/TO/MY/PROJECT
+PKG_CONFIG="$HOME/go/bin/pkg-config-static" go build PATH/TO/MY/PROJECT
 ```
 
 It works not only with Go, but also with anything that understands variable `PKG_CONFIG`.
--- main.go
+++ main.go
@@ -13,20 +13,34 @@ func main() {
 		panic("not enough arguments")
 	}
 	switch os.Args[1] {
+	case "--cflags":
+		runPkgConfig(os.Args[1:]...)
 	case "--libs":
 		var out bytes.Buffer
-		cmd := exec.Command("pkg-config", append([]string{"pkg-config"}, os.Args[1:]...)...)
+		cmd := exec.Command("pkg-config", append([]string{"pkg-config", "--static"}, os.Args[1:]...)...)
 		cmd.Stdout = &out
 		cmd.Run()
 		if cmd.ProcessState.ExitCode() != 0 {
 			fmt.Fprintf(os.Stdout, "%s", out.String())
 			os.Exit(cmd.ProcessState.ExitCode())
 		}
-		fmt.Fprintf(os.Stdout, "-Wl,-Bstatic %s -Wl,-Bdynamic\n", strings.Trim(out.String(), "\n"))
+		var result []string
+		for _, w := range strings.Split(strings.Trim(out.String(), "\n"), " ") {
+			switch {
+			case strings.HasPrefix(w, "-f"):
+				continue
+			}
+			result = append(result, w)
+		}
+		fmt.Fprintf(os.Stdout, "-Wl,-Bstatic %s -Wl,-Bdynamic\n", strings.Join(result, " "))
 		os.Exit(0)
 	default:
-		cmd := exec.Command("pkg-config", append([]string{"pkg-config"}, os.Args[1:]...)...)
-		cmd.Run()
-		os.Exit(cmd.ProcessState.ExitCode())
+		panic(fmt.Errorf("%v", os.Args))
 	}
+}
+
+func runPkgConfig(args ...string) {
+	cmd := exec.Command("pkg-config", append([]string{"pkg-config"}, args...)...)
+	cmd.Run()
+	os.Exit(cmd.ProcessState.ExitCode())
 }
